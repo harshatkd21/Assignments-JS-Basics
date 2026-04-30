1dc342c926842e4ae38a0f4afbdd97c7fa90275d
harshatkd21 <harutkd07@gmail.com>
2026-05-01 01:24:15 +0530

    Add files via upload
    Added comments for code lines

--- Assignment-OddorEven.js
+++ Assignment-OddorEven.js
@@ -2,35 +2,32 @@
 //number is odd and `"Even"` if the number is even.
 
 //create a function 
-/* function isOddOrEven(number){
-     
+ function OddOrEven(number){
+     // divide the number by 2 and save the remainder in x
     let x=number%2
-     console.log(x)
-
-
     if(x===1){
-        console.log("This is a odd number")
+        console.log(number+" -This is a odd number") // print if the remainder is 1
     }
     else{
-        console.log("This is a even number")
+        console.log(number+" -This is a even number") //print if the remainder is not 1
     }
 }
 
-isOddOrEven("2") */
+OddOrEven("5457") //calling the function with the parameter value as 2
 
 function isoddoreven(){
-    for(let i=0;i<=25;i++){
-     //   console.log(i);
-        let x= i%2
+    for(let i=0;i<=25;i++){  // looping till the value of i is less then or equal to 25.
+     //   init the value of i to 0
+        let x= i%2     // divide the value of i with 2 and save the value in x  // using modulus operator
         console.log("current number",i)
         
-        if(x===1){
-            console.log(i,"is a odd number")
+        if(x===1){                              // using strict operator
+            console.log(i,"is a odd number")  // print if the remainder is 1
         }
         else{
-            console.log(i,"is a even number")
+            console.log(i,"is a even number") // print if the remainder is not 1
         }
     }
 }
 
-isoddoreven()
+isoddoreven() //calling the function outside the scope.
--- Assignment-datatypes.js
+++ Assignment-datatypes.js
@@ -2,21 +2,26 @@
 //including an uninitialized variable. 
 
 var firstname='harsha'
-console.log(firstname)
-console.log(typeof(firstname))
+console.log(firstname)           // String
+console.log(typeof(firstname))   // typeof is used to determine the datatype of the given value.
 
 var companyName='testleaf'
 console.log(companyName)
-console.log(typeof(companyName))
+console.log(typeof(companyName))    // String
 
 var mobileNumber = 9954623185
 console.log(mobileNumber)
-console.log(typeof(mobileNumber))
+console.log(typeof(mobileNumber))   // integer
 
 var isAutomation = true
 console.log(isAutomation)
-console.log(typeof(isAutomation))
+console.log(typeof(isAutomation))   // boolean
 
 var hasPlaywright
 console.log(hasPlaywright)
-console.log(typeof(hasPlaywright))
+console.log(typeof(hasPlaywright))  // undefined
+
+var Playwright=null
+console.log(Playwright)
+console.log(typeof(Playwright))  // it’s a historical bug in JavaScript.
+
